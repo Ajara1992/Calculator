8e05081374ba6f260bc478456bd780aeaeee25d1
Ajara1992 <ajara.azamat@gmail.com>
2019-01-22 13:34:15 -0600

    divide method added

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,7 +12,7 @@ public  static double substract(double num1, double num2 ){
     {
     return num1 * num2;
     }
-    piblic static double divide(double num1, double num2){
+    public static  double divide(double num1, double num2){
     return num1 / num2; 
     }
 }
