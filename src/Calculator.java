d90a9e26cf4a3cf2329738c74ec1f6a4c53d4c34
Ajara1992 <33674183+Ajara1992@users.noreply.github.com>
2019-01-21 18:01:04 -0600

    add multiple method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,7 @@ public static double sum (double num1 , double num2){
 public  static double substract(double num1, double num2 ){
     return num1 - num2;
 }
+    public static double  multiply(double num1, double num2){
+    return num1 * num2;
+    }
 }
